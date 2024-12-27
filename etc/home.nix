fa949ee9c60c3b077288ffb3324428cded8c25d4
Cory Zibell <cory@zibell.cloud>
2024-12-27 12:14:27 +0800

    Don't newline starship

--- etc/home.nix
+++ etc/home.nix
@@ -167,7 +167,7 @@
 
     starship = { enable = true;
       settings = {
-        add_newline = true;
+        add_newline = false;
         character = { 
           success_symbol = "[➜](bold green)";
           error_symbol = "[➜](bold red)";
